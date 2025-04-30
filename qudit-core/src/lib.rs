b210e05e90bbe4dc116234d212d8cc9cbf71eb9b
Ed Younis <edyounis123@gmail.com>
2025-04-30 18:32:34 -0400

    Monorepo Init

---
+++ qudit-core/src/lib.rs
@@ -0,0 +1,37 @@
+#![warn(missing_docs)]
+
+//! Qudit-Core is the core package in the OpenQudit library.
+mod bitwidth;
+mod function;
+mod perm;
+mod radices;
+mod radix;
+mod scalar;
+mod system;
+
+pub mod accel;
+pub mod matrix;
+pub mod memory;
+pub mod unitary;
+
+pub use bitwidth::BitWidthConvertible;
+pub use function::HasBounds;
+pub use function::HasParams;
+pub use function::HasPeriods;
+pub use perm::calc_index_permutation;
+pub use perm::QuditPermutation;
+pub use radices::QuditRadices;
+pub use radices::ToRadices;
+pub use radix::ToRadix;
+pub use scalar::ComplexScalar;
+pub use scalar::RealScalar;
+pub use system::ClassicalSystem;
+pub use system::HybridSystem;
+pub use system::QuditSystem;
+
+////////////////////////////////////////////////////////////////////////
+/// Complex number types.
+////////////////////////////////////////////////////////////////////////
+pub use faer::c32;
+pub use faer::c64;
+////////////////////////////////////////////////////////////////////////
---
+++ qudit-core/src/matrix/mod.rs
@@ -0,0 +1,25 @@
+//! Matrix and Matrix-like types for use in the Openqudit library.
+
+mod matmat;
+mod matvec;
+
+/// Re-export of the macro constructors from the faer_core crate.
+pub use faer::mat;
+
+pub use faer::Col;
+pub use faer::Row;
+
+/// Re-export of the basic matrix types from the faer_core crate.
+pub use faer::Mat;
+pub use faer::MatMut;
+pub use faer::MatRef;
+
+/// Matrix Vector (3d tensor) type.
+pub use matvec::MatVec;
+pub use matvec::MatVecMut;
+pub use matvec::MatVecRef;
+
+/// Matrix Matrix (4d tensor) type.
+pub use matmat::SymSqMatMat;
+pub use matmat::SymSqMatMatMut;
+pub use matmat::SymSqMatMatRef;
