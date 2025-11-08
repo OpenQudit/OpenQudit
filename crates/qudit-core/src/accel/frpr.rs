99df3b5199a5c14cf9c6daf0cfc37089a003c974
Ed Younis <edyounis@lbl.gov>
2025-11-08 11:06:19 -0500

    Update

--- crates/qudit-core/src/accel/frpr.rs
+++ crates/qudit-core/src/accel/frpr.rs
@@ -1138,7 +1138,7 @@ pub fn fused_reshape_permute_reshape_into_prepare(
     // Apply permutation to input strides
     let perm_in_strides = perm
         .iter()
-        .map(|&p| in_strides[p] as isize)
+        .map(|&p| in_strides[p])
         .collect::<Vec<_>>();
 
     // (perm_in_strides, out_strides, perm_shape)
